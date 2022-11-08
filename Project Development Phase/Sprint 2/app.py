3df3c87db3d51debb13b55796ef00ec5db91f7b5
Shirley-Christabel <shirleychristabel01@gmail.com>
2022-11-08 19:54:59 +0530

    Sprint 2

--- Project Development Phase/Sprint 2/app.py
+++ Project Development Phase/Sprint 2/app.py
@@ -1,42 +1,28 @@
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('signin.html')
 
+@app.route('/signup')
+def signup_form():
+    return render_template('signup.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
-
 @app.route('/notifications')
 def notifications():
     return render_template('notifications.html')
 
-
 @app.route('/profile')
-def about():
+def profile():
     return render_template('profile.html')
 
-
-@app.route('/sign-in')
-def signin_form():
-    return render_template('sign-in.html')
-
-
-@app.route('/sign-up')
-def signup_form():
-    return render_template('sign-up.html')
-
-
-# @app.route('/tables')
-# def signup_form():
-#     return render_template('tables.html')
-
-
-# if __name__ == "__main__":
-#     app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
