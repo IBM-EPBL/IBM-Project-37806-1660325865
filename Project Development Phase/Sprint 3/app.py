288cc71e1c79e13d611aa18eb240bd59cce3a72b
veroni-d20 <veronishwetha.23it@licet.ac.in>
2022-11-08 20:41:50 +0530

    update ui

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -88,6 +88,14 @@ def create_user():
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route('/notifications')
+def notifications():
+    return render_template('notifications.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
