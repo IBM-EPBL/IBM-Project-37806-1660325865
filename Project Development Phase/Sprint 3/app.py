38cbbea0bb6eff4a73135a496fe30a301f5de523
veroni-d20 <veronishwetha.23it@licet.ac.in>
2022-11-08 19:04:25 +0530

    sprint 3

---
+++ Project Development Phase/Sprint 3/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('signin.html')
+
+@app.route('/signup')
+def signup_form():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
