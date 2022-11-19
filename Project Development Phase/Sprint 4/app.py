ea72eeeff7ef709c200ab7948cb6af0a6c552f63
veroni-d20 <veronishwetha.23it@licet.ac.in>
2022-11-19 11:20:32 +0530

    try except

--- Project Development Phase/Sprint 4/app.py
+++ Project Development Phase/Sprint 4/app.py
@@ -275,30 +275,33 @@ def profile():
         if not session.get("email"):
             return redirect("/")
         else:
-            email = session.get("email")
-            password = request.form['password']
-            interests = request.form['interests']
-            # converting password to array of bytes
-            bytes = password.encode('utf-8')
+            try:
+                email = session.get("email")
+                password = request.form['password']
+                interests = request.form['interests']
+                # converting password to array of bytes
+                bytes = password.encode('utf-8')
 
-            # generating the salt
-            salt = bcrypt.gensalt()
+                # generating the salt
+                salt = bcrypt.gensalt()
 
-            # Hashing the password
-            hashed_password = bcrypt.hashpw(bytes, salt)
+                # Hashing the password
+                hashed_password = bcrypt.hashpw(bytes, salt)
 
-            sql = "SELECT first_name, last_name, email FROM users WHERE email =?"
-            stmt = ibm_db.prepare(conn, sql)
-            ibm_db.bind_param(stmt, 1, email)
-            ibm_db.execute(stmt)
+                sql = "SELECT first_name, last_name, email FROM users WHERE email =?"
+                stmt = ibm_db.prepare(conn, sql)
+                ibm_db.bind_param(stmt, 1, email)
+                ibm_db.execute(stmt)
 
-            update_sql = "UPDATE USERS SET PASSWORD = ?, INTERESTS = ? WHERE email = ?"
-            prep_stmt = ibm_db.prepare(conn, update_sql)
-            ibm_db.bind_param(prep_stmt, 1, hashed_password)
-            ibm_db.bind_param(prep_stmt, 2, interests)
-            ibm_db.bind_param(prep_stmt, 3, email)
-            ibm_db.execute(prep_stmt)
-            return redirect("/dashboard", code=302)
+                update_sql = "UPDATE USERS SET PASSWORD = ?, INTERESTS = ? WHERE email = ?"
+                prep_stmt = ibm_db.prepare(conn, update_sql)
+                ibm_db.bind_param(prep_stmt, 1, hashed_password)
+                ibm_db.bind_param(prep_stmt, 2, interests)
+                ibm_db.bind_param(prep_stmt, 3, email)
+                ibm_db.execute(prep_stmt)
+                return redirect("/dashboard", code=302)
+            except e:
+                return redirect("/dashboard", code=302)
     elif request.method == 'GET':
         if not session.get("email"):
             return redirect("/")
